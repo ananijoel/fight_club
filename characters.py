850da867bb9db63f23011c3244ca1a0cde4ca9a8
anatide anani <ananijoellanatide@gmail.com>
2023-11-30 13:43:08 +0000

    fight club two players 2.0 - two players mode - tree actions possible - errors gestion

--- characters.py
+++ characters.py
@@ -1,17 +1,27 @@
 import random
+#import main
+
+ #TODO
+ # capitaliser le sexe pour ne plus avoir a faire une double verification sur le if
+
 class character :
-    def __init__(self,name,sexe,health,strength,number_of_health_potion,side):
+    def __init__(self, name, sexe, health, min_strength,max_strength, number_of_health_potion, side, endurance, choice=None):
         self.name = name.capitalize()
         self.sexe = sexe
         self.health = health
-        self.strength = strength
+        self.min_strength = min_strength
+        self.max_strength = max_strength
         self.number_of_health_potion  = number_of_health_potion
         self.is_alive = is_alive
         self.side = side
+        self.endurance = endurance
+        self.choice = choice
         if self.sexe == "M":
             self.sexe = "il"
         elif self.sexe == "F":
             self.sexe = "elle"
+
+
 def is_alive(character) :
     if character.health > 0:
         character.is_alive = True
@@ -22,8 +32,19 @@ def is_alive(character) :
 
 def Introduction(character) :
     print("Dans le coin "+str(character.side)+" du ring nous avons :")
-    print(character.name + " avec une force de frappe maxiamle de " + str(character.strength) + " points.")
+    print(character.name + " avec une force de frappe maxiamle de " + str(character.max_strength) + " points.")
     print("Et une santé s'elevant a " + str(character.health) + " points\n")
+
+def endurance_state(character):
+    passif = 1
+    defense = 0.5
+    actif = 1
+    if character.choice == 0 :
+        character.endurance += passif
+    elif character.choice  == 1 :
+        character.endurance -= defense
+    elif character.choice == 2 :
+        character.endurance -= actif
 
 
 def welcome_message(character) :
@@ -39,12 +60,68 @@ def healing(character):
         print(str(character.name)+"a utilisé une potion. Il a gagné "+str(potion_effect)+" de point de vie. Il a maintenant "+str(character.health)+" de point de vie ")
     else:
         print("vous n'avez plus de potion")
+
 def strike(character,character_2):
-    damage = random.randrange(0, character.strength)
-    character_2.health = character_2.health - damage
-    if character.health > 0  and character_2.health > 0:
-        print(str(character.name)+" a infligé "+str(damage)+" de degat a "+str(character_2.name)+" "+str(character.sexe)+" a maintenant  "+str(character_2.health)+" de points de vie")
-    elif  character.health > 0  and character_2.health < 0:
-        print(str(character.name)+" a infligé "+str(damage)+" de degat a "+str(character_2.name)+" il a maintenant 0 de points de vie ")
-    elif character.health < 0  and character_2.health >  0:
-        print(str(character.name) + " a infligé " + str(damage) + " de degat a " + str(character_2.name) + " il a maintenant "+str(character_2.health)+" de points de vie ")
+    endurance_state(character)
+    if character.endurance >= 0 :
+        damage = random.randrange(character.min_strength, character.max_strength)
+        character_2.health = character_2.health - damage
+        print("----------------")
+        print(str(character.name)+" a infligé "+str(damage)+" de degat a "+str(character_2.name))
+        print(str(character.name) + " a perdu 1 de son endurance. Maintenant, son endurance est de " + str(character.endurance))
+    else :
+        character.endurance = 0
+        print(str(character.name)+" n'a plus assez d'endurance pour defendre")
+
+
+def defense (character,character_2) :
+    endurance_state(character)
+    if character.endurance >= 0 :
+        print("----------------")
+        print(str(character.name) + " a choisi la defense.")
+        print(str(character.name) + " a perdu 0.5 de son endurance. Maintenant, son endurance est de " + str(character.endurance))
+        print(str(character.name)+" n'a recu aucun degat pv = "+str(character.health)+"\n")
+    else :
+        character.endurance = 0
+        print(str(character.name)+" n'a plus assez d'endurance pour defendre")
+def defense_against_strike(character,character_2) :
+    endurance_state(character)
+    endurance_state(character_2)
+    if character.endurance >= 0 :
+        print("----------------")
+        print(str(character.name) + " a choisi la defense.")
+        print(str(character.name) + " a perdu 0.5 de son endurance. Maintenant, son endurance est de " + str(character.endurance))
+        print(str(character_2.name)+" a attaqué "+str(character.name)+" ")
+        print(str(character_2.name)+" a perdu 1 point de son endurance. Maintenant, son endurance est de " + str(character_2.endurance))
+        print(str(character.name)+" n'a recu aucun degat pv = "+str(character.health)+"\n")
+    else:
+        character.endurance =0
+        strike(character_2,character)
+        defense(character,character_2)
+
+
+def passivity(character):
+    endurance_state(character)
+    print("----------------")
+    print(str(character.name) + " a choisi la passivité.")
+    print(str(character.name) + " a gagné 1 d'endurance. Maintenant, son endurance est de " + str(character.endurance))
+def health_printer(character,character_2):
+
+    if is_alive(character) and is_alive(character_2) :
+        print("*** Etat des joueurs a la fin du tour ***")
+        print(str(character.name)+" a "+str(character.health)+" de point de vie restant")
+        print(str(character_2.name) + " a " + str(character_2.health) + " de point de vie restant")
+        print("*************************************************************************")
+
+
+    elif is_alive(character) and not is_alive(character_2) :
+        print(str(character.name)+" a gagné")
+        print("*************************************************************************")
+
+    elif  not is_alive(character) and is_alive(character_2) :
+        print(str(character_2.name) + " a gagné")
+        print("*************************************************************************")
+
+    elif not is_alive(character) and  not is_alive(character_2):
+        print("Match null")
+        print("*************************************************************************")
--- main.py
+++ main.py
@@ -1,38 +1,82 @@
 import random
 import characters
+import getpass
+
 print("*** Bienvenu dans le jeu dual fight ***\n")
-# definition et instanciation des propiétés de l'objet du premier user
-#jedi_user_name = input("veuillez entrer le nom de votre avatar\nreponse : ")
-#jedi_sexe  = input("quel est votre sexe ?(M/F)\nreponse : ")
-jedi = characters.character("joel", "M", 100, 10, 3,"gauche")
-# message d'acceuil pour le premier user
+
+jedi_user_name = input("veuillez entrer le nom de votre avatar\nreponse : ")
+jedi_sexe  = input("quel est votre sexe ?(M/F)\nreponse : ")
+jedi = characters.character(jedi_user_name,jedi_sexe,50, 5,15, 3,"gauche",1)
 characters.welcome_message(jedi)
 
-# definition et instanciation des propiétés de l'objet du second user
-#sith_user_name = input("veuillez entrer le nom de votre avatar\nreponse : ")
-#sith_sexe  = input("quel est votre sexe ?(M/F)\nreponse : ")
-sith = characters.character("elie", "F", 150, 15, 0,"droit")
-# message d'acceuil pour le second user
+
+ith_user_name = input("veuillez entrer le nom de votre avatar\nreponse : ")
+sith_sexe  = input("quel est votre sexe ?(M/F)\nreponse : ")
+sith = characters.character(ith_user_name,sith_sexe,100, 5,15, 0,"droit",1)
 characters.welcome_message(sith)
 
-print("Medames et messieur bienvenus dans l'arene du dual fight. Ce soir : \n".upper())
-
+print("\nMedames et messieur bienvenus dans l'arene du dual fight. Ce soir : \n".upper())
 characters.Introduction(jedi)
-
 characters.Introduction(sith)
 
-"""
-while characters.is_alive(jedi) and characters.is_alive(sith):
-    user_choice = input("souhaitez vous attaquer (1) ou utiliser une potion (2) ? \nreponse : ")
-    if user_choice.isdigit() and int(user_choice) == 1:
+print("Voici les mecaniques du jeu".upper())
+print("A  chaque tour, vous pouvez choisir entre 0,1, et 2")
+print("avec 0 vous pouvez rester passif  et vous gagnez un point (1) d'endurance mais vous prenez de plein fouet le coup enemi. ")
+print("Avec 1, vous optez pour une defense qui vous permet d'encaiiser le coup enemie sans perdre en point de vie mais vous perdez O.5 points en endurance ")
+print("Avec 2, vous optez pour une attaque dont le force sera compris au hazard entre la valeur de frappe minimale et maximale de votre avatar")
+print("le premier a O point de vie aura perdu le combat")
+
+
+
+while characters.is_alive(jedi) and characters.is_alive(sith) :
+    while jedi.choice != "0" and jedi.choice != "1" and jedi.choice != "2" :
+        jedi.choice = getpass.getpass("que choisis "+str(jedi.name)+" ?\n reponse :")
+    while sith.choice != "0" and sith.choice != "1" and sith.choice != "2":
+        sith.choice = getpass.getpass("que choisis "+str(sith.name)+"?\n reponse :")
+    jedi.choice = int(jedi.choice)
+    sith.choice = int(sith.choice)
+    if jedi.choice == 0 and sith.choice == 0:
+        characters.passivity(jedi)
+        characters.passivity(sith)
+        characters.health_printer(jedi,sith)
+    elif jedi.choice == 0 and sith.choice == 1:
+        characters.passivity(jedi)
+        characters.defense(sith,jedi)
+        characters.health_printer(jedi,sith)
+    elif jedi.choice == 0 and sith.choice == 2:
+        characters.passivity(jedi)
+        characters.strike(sith,jedi)
+        characters.health_printer(jedi,sith)
+
+
+    elif jedi.choice == 1 and sith.choice == 0:
+        characters.defense(jedi,sith)
+        characters.passivity(sith)
+        characters.health_printer(jedi,sith)
+
+    elif jedi.choice == 1 and sith.choice == 1:
+        characters.defense(jedi, sith)
+        characters.defense(sith, jedi)
+        characters.health_printer(jedi,sith)
+
+    elif jedi.choice == 1 and sith.choice == 2:
+        #characters.strike(sith,jedi)
+        characters.defense_against_strike(jedi,sith)
+        characters.health_printer(jedi,sith)
+
+
+    elif jedi.choice == 2 and sith.choice == 0:
+            characters.strike(jedi,sith)
+            characters.passivity(sith)
+            characters.health_printer(jedi, sith)
+
+    elif jedi.choice == 2 and sith.choice == 1:
+        characters.defense_against_strike(sith, jedi)
+        characters.health_printer(jedi,sith)
+
+    elif jedi.choice == 2 and sith.choice == 2:
         characters.strike(jedi,sith)
         characters.strike(sith,jedi)
-    elif user_choice.isdigit() and int(user_choice) == 2:
-        characters.healing(jedi)
-        characters.strike(sith,jedi)
+        characters.health_printer(jedi,sith)
 
-if characters.is_alive(jedi) :
-    print("vous avez gagné\nFelicitations!!")
-else:
-    print("vous avez perdu\nGros LOOSER")
-"""
+print("\n*** fin du jeu ***\n")
